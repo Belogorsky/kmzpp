559605fc92a89c5d31bf7a408655aaf5f128360c
Belogorsky <northbel@yandex.ru>
2015-05-06 11:01:18 +0300

    Second commit. Check class.

--- .idea/src/test.java
+++ .idea/src/test.java
@@ -21,4 +21,33 @@ public class test {
        // write("out.txt", lines);
 
     }
+
+    public static boolean checkWithRegExpName(String testString){
+        Pattern name = Pattern.compile("^[А-Я]{1}[а-я]{3,15}$");
+
+        Matcher match = name.matcher(testString);
+        while(match.find()) {
+            System.out.println(testString.substring(match.start(), match.end()));
+        }
+        return match.matches();
+    }
+
+    public static boolean checkWithRegExpPhone(String testString){
+        Pattern phone = Pattern.compile("^[+]{0,1}[- (),0-9]{7,}$");
+        Matcher match = phone.matcher(testString);
+        while(match.find()) {
+            System.out.println(testString.substring(match.start(), match.end()));
+        }
+        return match.matches();
+    }
+
+    public static boolean checkWithRegExpEmail(String testString){
+        Pattern phone = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        Matcher match = phone.matcher(testString);
+        while(match.find()) {
+            System.out.println(testString.substring(match.start(), match.end()));
+        }
+        return match.matches();
+    }
 }
